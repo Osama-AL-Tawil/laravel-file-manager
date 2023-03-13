d45fea4163209c1f750236c1322ed7b54c5076d2
Osama <Osama20019>
2023-03-13 14:29:35 +0200

    full update

--- src/LaravelFileManagerServiceProvider.php
+++ src/LaravelFileManagerServiceProvider.php
@@ -7,11 +7,19 @@ use Illuminate\Support\ServiceProvider;
 class LaravelFileManagerServiceProvider extends ServiceProvider
 {
     public function boot(){
-
-        $this->publishes([
-            __DIR__.'/../src/database/migrations/create_files_table.php.stub'=>
-            $this->app->databasePath('migrations/'.date('Y_m_d_His', time()).'_create_files_table.php'),
-        ],'migrations');
+        if ($this->app->runningInConsole()) {
+            // Export the migration
+            if (! class_exists('CreatePostsTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
+                    // you can add any number of migrations here
+                ], 'migrations');
+            }
+        }
+//        $this->publishes([
+//            __DIR__.'/../src/database/migrations/create_files_table.php.stub'=>
+//            $this->app->databasePath('migrations/'.date('Y_m_d_His', time()).'_create_files_table.php'),
+//        ],'migrations');
 
         //$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
