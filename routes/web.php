fcd98798f55cea8370df93dcd39a4ff7bd2db760
Osama <Osama20019>
2023-03-13 23:52:46 +0200

    Exception Handling

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,19 @@ use Illuminate\Support\Facades\Storage;
 if (config('laravel_file_manager.disk') == 'files') {
     Route::get('/files/{path}', function ($path) {
 
-        $disk = config('laravel_file_manager.disk');
-        $is_encrypted = config('laravel_file_manager.encrypted_url');
+        try {
+            $disk = config('laravel_file_manager.disk');
+            $is_encrypted = config('laravel_file_manager.encrypted_url');
 
-        $file_path = $is_encrypted ? Crypt::decryptString($path) : $path;
+            $file_path = $is_encrypted ? Crypt::decryptString($path) : $path;
 
-        if (Storage::disk($disk)->exists($file_path)) {
-            return Storage::disk($disk)->response($file_path);
-        } else {
-            abort(404, 'File Not Found');
+            if (Storage::disk($disk)->exists($file_path)) {
+                return Storage::disk($disk)->response($file_path);
+            } else {
+                abort(404, 'File Not Found');
+            }
+        }catch (Exception $exception){
+            abort($exception->getCode(), $exception->getMessage());
         }
     })->where('path', '.*');
 
