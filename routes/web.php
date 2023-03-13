61e186fb742d008b3f617ea5e61ee79d30e5dfc4
Osama <Osama20019>
2023-03-13 23:35:09 +0200

    new

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 if (config('laravel_file_manager.disk') == 'files') {
     Route::get('/files/{path}', function ($path) {
-        return \OST\LaravelFileManager\FileManager::getFileByPath($path);
+
+        $disk = config('laravel_file_manager.disk');
+        $is_encrypted = config('laravel_file_manager.encrypted_url');
+
+        $file_path = $is_encrypted ? Crypt::decryptString($path) : $path;
+
+        if (Storage::disk($disk)->exists($file_path)) {
+            return Storage::disk($disk)->response($file_path);
+        } else {
+            abort(404, 'File Not Found');
+        }
     })->where('path', '.*');
 
 }
